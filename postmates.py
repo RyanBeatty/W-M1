0b28ab0a2c2612a2408dab42a3725c86ac2aaf87
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2015-02-12 15:07:06 -0500

    some postmates API calls functioning

---
+++ postmates.py
@@ -0,0 +1,74 @@
+import httplib, urllib
+import requests
+import json
+
+def deliveryQuote():
+    url = 'https://api.postmates.com/v1/customers/cus_KAgGPlHZ6tZmzF/delivery_quotes'
+    headers = { 'Authorization':'Basic NWUzODM3Y2MtNzM0MS00MzRkLThlNGUtNTA2MjYwYTQyMjVkOg==',"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain" }
+    payload = {'pickup_address':'202 McAllister St. San Francisco, CA', 'dropoff_address':'101 Market St. San Francisco, CA'}
+    r = requests.post(url, data=payload, headers=headers)
+    return r.json()
+
+def deliveryPlace( quote_id ):
+    url = 'https://api.postmates.com/v1/customers/cus_KAgGPlHZ6tZmzF/deliveries'
+    headers = { 'Authorization':'Basic NWUzODM3Y2MtNzM0MS00MzRkLThlNGUtNTA2MjYwYTQyMjVkOg==',"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain" }
+    payload = {'manifest':'a box of kittens', 'manifest_reference' : 'Optional ref', 'pickup_name': 'The Warehouse',\
+     'pickup_address':'202 McAllister St. San Francisco, CA', \
+     'pickup_phone_number':'555-555-5555',\
+     'pickup_business_name':'Optional nameee',\
+     'pickup_notes':'Optional notes',\
+     'dropoff_name':'Alice',\
+     'dropoff_phone_number':'232-323-2222',\
+     'dropoff_business_name':'Optional Business name',\
+     'dropoff_address':'101 Market St. San Francisco, CA',\
+     'quote_id':quote_id}
+    r = requests.post(url, data=payload, headers=headers)
+    return r.json()
+
+# Lists all the deliveries ever placed by a user
+def deliveryListAll():
+    url = 'https://api.postmates.com/v1/customers/cus_KAgGPlHZ6tZmzF/deliveries'
+    # url = 'https://api.postmates.com/v1/customers/cus_KAgGPlHZ6tZmzF/deliveries?filter=ongoing'
+    headers = { 'Authorization':'Basic NWUzODM3Y2MtNzM0MS00MzRkLThlNGUtNTA2MjYwYTQyMjVkOg==',"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain" }
+    payload = {}
+    r = requests.get(url, data=payload, headers=headers)
+    return r.json()
+
+# Lists ongoing deliveries for a given user
+def deliveryListOngoing():
+    # url = 'https://api.postmates.com/v1/customers/cus_KAgGPlHZ6tZmzF/deliveries'
+    url = 'https://api.postmates.com/v1/customers/cus_KAgGPlHZ6tZmzF/deliveries?filter=ongoing'
+    headers = { 'Authorization':'Basic NWUzODM3Y2MtNzM0MS00MzRkLThlNGUtNTA2MjYwYTQyMjVkOg==',"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain" }
+    payload = {'filter':'ongoing'}
+    r = requests.get(url, data=payload, headers=headers)
+    return r.json()
+
+def deliveryDetails( delivery_id ):
+    url = 'https://api.postmates.com/v1/customers/cus_KAgGPlHZ6tZmzF/deliveries/'+delivery_id
+    headers = { 'Authorization':'Basic NWUzODM3Y2MtNzM0MS00MzRkLThlNGUtNTA2MjYwYTQyMjVkOg==',"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain" }
+    payload = {}
+    r = requests.get(url, data=payload, headers=headers)
+    return r.json()
+
+def deliveryCancel( delivery_id ):
+    url = 'https://api.postmates.com/v1/customers/cus_KAgGPlHZ6tZmzF/deliveries/'+ delivery_id + '/cancel'
+    headers = { 'Authorization':'Basic NWUzODM3Y2MtNzM0MS00MzRkLThlNGUtNTA2MjYwYTQyMjVkOg==',"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain" }
+    payload = {}
+    r = requests.post(url, data=payload, headers=headers)
+    return r.json()
+
+def deliveryReturn():
+    url = 'https://api.postmates.com/v1/customers/cus_KAgGPlHZ6tZmzF/deliveries/'+ delivery_id + '/return'
+    headers = { 'Authorization':'Basic NWUzODM3Y2MtNzM0MS00MzRkLThlNGUtNTA2MjYwYTQyMjVkOg==',"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain" }
+    payload = {}
+    r = requests.post(url, data=payload, headers=headers)
+    return r.json()
+
+def main():
+    deliveryInfo = deliveryQuote()
+    placedeliv = deliveryPlace(deliveryInfo['id'])
+    print(deliveryListOngoing())
+
+
+if __name__ == "__main__":
+    main()
