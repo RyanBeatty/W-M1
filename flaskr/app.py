d35d5ad27148f239e60eb300891e751a8146900f
Ryan Beatty <rvbeatty@email.wmd.edu>
2015-02-12 21:04:14 -0500

    added landing page and now use location

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,14 +1,19 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import rss_scraper
 import json
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
-	return render_template("index.html", index_list=rss_scraper.parse_rss_feed("norfolk"))
+	return render_template("index.html")
+
+@app.route("/search", methods=["GET"])
+def search():
+	site = request.args.get("site", "norfolk")
+	return render_template("search.html", index_list=rss_scraper.parse_rss_feed(site))
 
 @app.route("/item_list", methods=["GET"])
 def item_list():
