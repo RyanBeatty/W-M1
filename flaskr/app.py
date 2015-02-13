d9306547d8951f293b13d9c606550eb3d693979e
Ryan Beatty <rvbeatty@email.wmd.edu>
2015-02-12 19:22:26 -0500

    started api route for rss feed item list

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,6 +1,6 @@
 
 from flask import Flask, render_template
-
+import rss_scraper
 
 app = Flask(__name__)
 
@@ -8,6 +8,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 	return render_template("index.html")
+
+@app.route("/item_list", methods=["GET"])
+def item_list():
+	return "hello world"
 
 if __name__ == "__main__":
 	app.debug = True
