434ac0fc5bc800d1fd81306c7cbf69723e1472b6
Ryan Beatty <rvbeatty@email.wmd.edu>
2015-02-12 20:01:05 -0500

    lol

--- rss_scraper.py
+++ rss_scraper.py
@@ -1,4 +1,8 @@
+from bs4 import BeautifulSoup
+import requests
+import re
 import feedparser
+
 
 rss_generic_free_link = "http://%s.craigslist.org/search/zip?query=%s&format=rss"
 
@@ -23,6 +27,30 @@ def parse_item_list(rss):
 def parse_rss_feed(site, term=""):
 	rss = get_rss(site, term)
 	return parse_item_list(rss)
+
+
+# Get the html for a specific webpage
+
+def getItemHTML( url ):
+    resp = requests.get("http://" + url)
+    data = resp.text
+    return data
+
+# Parse the url with BeautifulSoup and return the relevant data
+
+def getItemData( url ):
+    data = getItemHTML( url )
+    soup = BeautifulSoup(data)                                #if theres nothing in the array no pic
+    mapwrap = soup.find_all(href=re.compile("maps.google.com"))    #if theres nothing in the array, no map
+    replylink = soup.find(id="replylink")
+    emailRequest = requests.get("http://norfolk.craigslist.com" + replylink.get('href'))
+    emailRequestData = emailRequest.text
+    emailSoup = BeautifulSoup(emailRequestData)
+    replyEmail = emailSoup.find_all(class_="anonemail")[0].text
+    returnData = { }
+    returnData['map'] = mapwrap
+    returnData['replyemail'] = replyEmail
+    return returnData
 
 if __name__ == "__main__":
 	parse_rss_feed()
